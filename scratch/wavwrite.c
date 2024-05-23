6c65eccd5566fcbeae9c7c7008010a7f0f5bd2d4
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-05-23 14:09:05 -0400

    logging while making prototype wav writer

--- scratch/wavwrite.c
+++ scratch/wavwrite.c
@@ -30,8 +30,15 @@ void write_master_chunk(FILE *wav)
     uint32_t cksize;
 
     /* 4 + n, n = 2 */
-    /* one chunk for format, one chunk for PCM data */
-    cksize = 4 + 2;
+    /* 4 constant */
+    cksize = 4;
+    /* 16: fmt chunk */
+    cksize += 16;
+    /* data chunk size */
+    cksize += 44096 * 2;
+
+    /* not sure, where these extra 16 bytes come from. */
+    cksize += 16;
     write_str(wav, "RIFF", 4);
     write_uint32(wav, cksize);
     write_str(wav, "WAVE", 4);
@@ -39,35 +46,74 @@ void write_master_chunk(FILE *wav)
 
 void write_fmt_chunk(FILE *wav)
 {
-    uint32_t cksize;
-
     write_str(wav, "fmt ", 4);
 
-    cksize = 0;
+    /* chunk size: 16 */
+    write_uint32(wav, 0x10);
+
     /* format code: WAVE_FORMAT_PCM (0x0001) */
     write_uint16(wav, 0x0001);
-    cksize += 2;
 
     /* nchannels: 1 (always mono) */
     write_uint16(wav, 0x0001);
-    cksize += 2;
 
     /* sampling rate: 44.kHz hardcoded for now */
     write_uint32(wav, 44100);
-    cksize += 4;
 
     /* average bytes per second: huh? */
-    /* constant bitrate 32-bit float at 44.1kHz */
-    write_uint32(wav, 44100 / 4);
-    cksize += 4;
+    /* goal: 16-bit at 44.1kHz @ 2 bytes/sample */
+    write_uint32(wav, 44100 * 2);
 
-    /* TODO: bits-per-second */
-    /* TODO: extension size */
-    /* TODO: valid bits per sample */
-    /* TODO: speaker position mask */
-    /* TODO: subformat */
+    /* block alignment of waveform data, in bytes. I think this is just 2 bytes? */
+    write_uint16(wav, 2);
 
-    printf("chunk size: %d\n", cksize);
+    /* bits-per-sample */
+    write_uint16(wav, 16);
+}
+
+void write_data_chunk(FILE *wav)
+{
+    uint32_t sz;
+
+    /* temporary hard-coded size nsamples * 2 bytes/samp */
+
+    sz = 44096 * 2;
+    write_str(wav, "data", 4);
+    write_uint32(wav, sz);
+}
+
+uint32_t get_nsamps(FILE *data)
+{
+    uint32_t nsamps;
+    fseek(data, 0L, SEEK_END);
+    nsamps = ftell(data);
+    nsamps /= 4; /* 32-bit float: 4 bytes per sample */
+    rewind(data);
+    return nsamps;
+}
+
+void write_data(FILE *wav, FILE *data)
+{
+    uint32_t nsamps, i;
+    uint32_t nbytes;
+
+    nsamps = get_nsamps(data);
+    printf("nsamps is now %d\n", nsamps);
+    nbytes = 0;
+
+    for (i = 0; i < nsamps; i++) {
+        /* convert from float to 16-bit signed int */
+        int16_t s16;
+        float f;
+
+        f = 0;
+
+        fread(&f, 1, 4, data);
+        s16 = f * 0x8000;
+        nbytes += fwrite(&s16, 1, 2, wav);
+    }
+
+    printf("%d bytes written\n", nbytes);
 }
 
 int main(int argc, char *argv[])
@@ -85,6 +131,8 @@ int main(int argc, char *argv[])
 
     write_master_chunk(wav);
     write_fmt_chunk(wav);
+    write_data_chunk(wav);
+    write_data(wav, fp);
 
     fclose(wav);
     fclose(fp);
