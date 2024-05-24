27e349322398e8abc1bf8ae64f92f3a662922190
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-05-24 08:53:50 -0400

    wavwrite finishing touches

--- scratch/wavwrite.c
+++ scratch/wavwrite.c
@@ -31,16 +31,16 @@ void write_master_chunk(FILE *wav)
 
     /* 4 + n, n = 2 */
     /* 4 constant */
-    cksize = 4;
+    /* cksize = 4;
     /* 16: fmt chunk */
-    cksize += 16;
+    /* cksize += 16; */
     /* data chunk size */
-    cksize += 44096 * 2;
+    /* cksize += 44096 * 2; */
 
     /* not sure, where these extra 16 bytes come from. */
-    cksize += 16;
+    /* cksize += 16; */
     write_str(wav, "RIFF", 4);
-    write_uint32(wav, cksize);
+    write_uint32(wav, 0);
     write_str(wav, "WAVE", 4);
 }
 
@@ -73,34 +73,17 @@ void write_fmt_chunk(FILE *wav)
 
 void write_data_chunk(FILE *wav)
 {
-    uint32_t sz;
-
-    /* temporary hard-coded size nsamples * 2 bytes/samp */
-
-    sz = 44096 * 2;
     write_str(wav, "data", 4);
-    write_uint32(wav, sz);
+    /* to be written later */
+    write_uint32(wav, 0);
 }
 
-uint32_t get_nsamps(FILE *data)
+uint32_t write_data(FILE *wav, FILE *data)
 {
-    uint32_t nsamps;
-    fseek(data, 0L, SEEK_END);
-    nsamps = ftell(data);
-    nsamps /= 4; /* 32-bit float: 4 bytes per sample */
-    rewind(data);
-    return nsamps;
-}
-
-void write_data(FILE *wav, FILE *data)
-{
-    uint32_t nsamps, i;
     uint32_t nbytes;
 
-    nsamps = get_nsamps(data);
-    printf("nsamps is now %d\n", nsamps);
     nbytes = 0;
-
+#if 0
     for (i = 0; i < nsamps; i++) {
         /* convert from float to 16-bit signed int */
         int16_t s16;
@@ -112,27 +95,63 @@ void write_data(FILE *wav, FILE *data)
         s16 = f * 0x8000;
         nbytes += fwrite(&s16, 1, 2, wav);
     }
+#endif
+
+    rewind(data);
+    while (!feof(data)) {
+        /* convert from float to 16-bit signed int */
+        int16_t s16;
+        float f;
+        int bytes_read;
+        f = 0;
+        bytes_read = fread(&f, 1, 4, data);
+        if (bytes_read< 4) break;
+        s16 = f * 0x8000;
+        nbytes += fwrite(&s16, 1, 2, wav);
+    }
 
     printf("%d bytes written\n", nbytes);
+    return nbytes;
+}
+
+void update_chunks(FILE *wav, uint32_t nbytes)
+{
+    /* update RIFF chunk size */
+    fseek(wav, 0x04, SEEK_SET);
+
+    write_uint32(wav, nbytes + 16 + 16 + 4);
+
+    /* update data size */
+    fseek(wav, 0x28, SEEK_SET);
+
+    write_uint32(wav, nbytes);
 }
 
 int main(int argc, char *argv[])
 {
     FILE *fp;
     FILE *wav;
-    uint32_t nsamps;
+    const char *wav_fname;
+    const char *data_fname;
 
-    fp = fopen("test.bin", "r");
-    wav = fopen("test.wav", "w");
-    fseek(fp, 0L, SEEK_END);
-    nsamps = ftell(fp);
-    nsamps /= 4; /* 32-bit float: 4 bytes per sample */
-    printf("nsamps: %d\n", nsamps);
+    uint32_t nbytes;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s in.bin out.wav\n", argv[0]);
+        return 1;
+    }
+
+    data_fname = argv[1];
+    wav_fname = argv[2];
+
+    fp = fopen(data_fname, "r");
+    wav = fopen(wav_fname, "w");
 
     write_master_chunk(wav);
     write_fmt_chunk(wav);
     write_data_chunk(wav);
-    write_data(wav, fp);
+    nbytes = write_data(wav, fp);
+    update_chunks(wav, nbytes);
 
     fclose(wav);
     fclose(fp);
