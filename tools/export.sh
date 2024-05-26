b739c7df4d93925ec06a6a17a7e83dec0db70572
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-05-26 12:09:21 -0400

    created dagzet directories in export script

--- tools/export.sh
+++ tools/export.sh
@@ -1,5 +1,8 @@
 mkdir -p _site/recurse
 mkdir -p _site/recurse/css
+
+# generate directory structure for dagzet
+find data/* -type d | sed "s/^data\///" | xargs -I {} mkdir -p _site/recurse/dz/{}
 weewiki sync
 weewiki export
 rsync -rvt css/style.css _site/recurse/css
