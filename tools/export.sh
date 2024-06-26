15110ce5da1aa346a5220dd7cff5c18f330969f3
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-06-26 16:42:19 -0400

    initial working HTMLized code files

--- tools/export.sh
+++ tools/export.sh
@@ -4,6 +4,8 @@ mkdir -p _site/recurse/css
 # generate directory structure for dagzet
 find data/* -type d | sed "s/^data\///" | xargs -I {} mkdir -p _site/recurse/dz/{}
 
+echo "SELECT distinct(filename) from dz_textfiles" | sqlite3 a.db | xargs -I {} mkdir -p _site/recurse/dzf/{}
+
 # task management
 mkdir -p _site/recurse/tasks
 mkdir -p _site/recurse/taskgroups
@@ -11,3 +13,4 @@ mkdir -p _site/recurse/taskgroups
 weewiki sync
 weewiki export
 rsync -rvt css/style.css _site/recurse/css
+rsync -rvt css/code.css _site/recurse/css
