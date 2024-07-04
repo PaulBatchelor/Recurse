f7d7c5cace2950d7a32df0717cacf0134708dbc5
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-04 16:01:11 -0400

    missing file

---
+++ scratch/poke/chatter.js
@@ -0,0 +1,38 @@
+class ChatterSynth extends AudioWorkletProcessor {
+    constructor(options) {
+        super(options)
+        const wasmBytes = options.processorOptions.wasmBytes;
+        const mod = new WebAssembly.Module(wasmBytes);
+        this.wasm = new WebAssembly.Instance(mod, {});
+        //this.gpath = this.wasm.exports.genseq();
+        this.dsp = this.wasm.exports.newdsp(sampleRate);
+        this.outptr = this.wasm.exports.alloc(128);
+        this.outbuf = new Float32Array(this.wasm.exports.memory.buffer,
+                this.outptr,
+                128);
+
+        this.port.onmessage = (event) => this.onmessage(event.data);
+    }
+
+    process(inputs, outputs, parameters) {
+        const output = outputs[0];
+        this.wasm.exports.process(this.dsp, this.outptr, 128);
+        for (let channel = 0; channel < output.length; ++channel) {
+            const outputChannel = output[channel];
+            for (let i = 0; i < outputChannel.length; ++i) {
+                outputChannel[i] = this.outbuf[i];
+            }
+        }
+
+        return true;
+    }
+
+    onmessage(event) {
+        if (event.type === "poke") {
+            this.wasm.exports.poke(this.dsp);
+        }
+    }
+
+}
+
+registerProcessor('chatter', ChatterSynth);
