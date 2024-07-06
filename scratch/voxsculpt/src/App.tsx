1f8c0fff6651a36d4510b489f80cab0afb4855b7
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-06 19:23:06 -0400

    added slider groups to react slider controllers

--- scratch/voxsculpt/src/App.tsx
+++ scratch/voxsculpt/src/App.tsx
@@ -10,26 +10,87 @@ interface SliderProps {
     label: string;
 }
 
-const parameters = [
+interface SliderGroupProps {
+    label: string;
+    sliders: SliderProps[];
+}
+
+const glottis_parameters = [
     {
         name: "freq",
         label: "Frequency",
-        minRange: 20.0,
-        maxRange: 80.0,
-        defaultValue: 63.0,
-        stepSize: 0.01,
+        minRange: 26.0,
+        maxRange: 86.0,
+        defaultValue: 65.0,
+        stepSize: 1,
     },
     {
-        name: "amp",
-        label: "Amplitude",
+        name: "aspiration",
+        label: "Aspiration",
         minRange: 0.0,
         maxRange: 1.0,
-        defaultValue: 0.7,
-        stepSize: 0.01,
+        defaultValue: 0.03,
+        stepSize: 0.001,
+    },
+    {
+        name: "noise_floor",
+        label: "Noise Floor",
+        minRange: 0.0,
+        maxRange: 1.0,
+        defaultValue: 0.01,
+        stepSize: 0.001,
+    },
+    {
+        name: "shape",
+        label: "Shape",
+        minRange: 0.1,
+        maxRange: 0.9,
+        defaultValue: 0.4,
+        stepSize: 0.001,
+    },
+    {
+        name: "velum",
+        label: "Velum",
+        minRange: 0.0,
+        maxRange: 4.0,
+        defaultValue: 0.0,
+        stepSize: 0.001,
     },
 ];
 
-function Slider({ name, minRange, maxRange, stepSize, label, defaultValue }: SliderProps) {
+const tract_parameters = [
+    {
+        name: "length",
+        label: "Tract Length",
+        minRange: 8.0,
+        maxRange: 30.0,
+        defaultValue: 14.0,
+        stepSize: 0.1,
+    },
+];
+
+function generateRegionParams() {
+    let p = []
+
+
+    for (let i = 1; i <= 8; i++) {
+        p.push({
+            name: "region" + i,
+            label: "Region " + i,
+            minRange: 0.0,
+            maxRange: 4.0,
+            defaultValue: 0.5,
+            stepSize: 0.001,
+        });
+    }
+
+
+    return p;
+}
+
+const tract_region_params = generateRegionParams();
+
+function Slider({ minRange, maxRange, stepSize, label, defaultValue }: SliderProps) {
     const [param, setParam] = React.useState(defaultValue);
 
     function handleChange(e: React.FormEvent<HTMLInputElement>) {
@@ -55,11 +116,16 @@ function Slider({ name, minRange, maxRange, stepSize, label, defaultValue }: Sli
 
 }
 
-function App() {
-    var sliders: React.JSX.Element[] = [];
+function SliderGroup({ label, sliders }: SliderGroupProps) {
 
-    parameters.forEach((param) => {
-        sliders.push(
+    var slidersJSX: React.JSX.Element[] = [];
+
+    slidersJSX.push(
+        <h2>{label}</h2>
+    );
+
+    sliders.forEach((param) => {
+        slidersJSX.push(
             <Slider
                 key={param.name}
                 name={param.name}
@@ -74,7 +140,26 @@ function App() {
 
     return (
         <div className="App">
-            {sliders}
+            {slidersJSX}
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="App">
+            <SliderGroup
+                label="Glottis"
+                sliders={glottis_parameters}
+            />
+            <SliderGroup
+                label="Tract"
+                sliders={tract_parameters}
+            />
+            <SliderGroup
+                label="Tract Regions"
+                sliders={tract_region_params}
+            />
         </div>
     );
 }
