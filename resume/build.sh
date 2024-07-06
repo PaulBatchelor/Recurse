5eaf20deae166c652122bcffbb9c40c70edb94c1
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-06 16:45:36 -0400

    added initial tex backend

--- resume/build.sh
+++ resume/build.sh
@@ -1,6 +1,8 @@
-> resume.db
-cd ..
-./tools/dagzet resume/resume.dz | sqlite3 resume/resume.db
-cd -
+# > resume.db
+# cd ..
+# ./tools/dagzet resume/resume.dz | sqlite3 resume/resume.db
+# cd -
 mnolth lua resume.lua
-pandoc -o out.html out.md
+tex out.tex
+dvipdfm out.dvi
+#pandoc -o out.html out.md
--- resume/resume.lua
+++ resume/resume.lua
@@ -1,5 +1,6 @@
 db = sqlite3.open("resume.db")
 json = dofile("../tools/json.lua")
+tex_backend = require("tex")
 
 function get_node_id(db, name)
     local stmt = db:prepare(
@@ -67,33 +68,39 @@ function get_node_lines(db, nid)
     return table.concat(json.decode(lines_txt), " ")
 end
 
+markdown_backend = {
+    prelude = function() end,
 
-function header1(str)
-    return "# " .. str .. "\n"
-end
+    epilogue = function() end,
 
-function header2(str)
-    return "## " .. str .. "\n"
-end
+    header1 = function (str)
+        return "# " .. str .. "\n"
+    end,
 
-function header3(str)
-    return "### " .. str .. "\n"
-end
+    header2 = function(str)
+        return "## " .. str .. "\n"
+    end,
 
-function begin_list()
-    return ""
-end
+    header3 = function (str)
+        return "### " .. str .. "\n"
+    end,
 
-function end_list()
-    return "\n"
-end
+    begin_list = function ()
+        return ""
+    end,
 
-function item(str)
-    return "- " .. str .. "\n"
-end
+    end_list = function()
+        return "\n"
+    end,
 
-function experience(fp)
-    fp:write(header2("Experience"))
+    item = function (str)
+        return "- " .. str .. "\n"
+    end
+}
+
+
+function experience(fp, backend)
+    fp:write(backend.header2("Experience"))
     local experience_name = "resume/experience"
     local nid = get_node_id(db, experience_name)
     assert(nid > 0,
@@ -104,22 +111,22 @@ function experience(fp)
 
     for _, v in pairs(explst) do
         local title = get_node_lines(db, v)
-        fp:write(header3(title))
+        fp:write(backend.header3(title))
         local points = get_node_list(db, v)
 
         if #points > 0 then
-            fp:write(begin_list())
+            fp:write(backend.begin_list())
             for _, li in pairs(points) do
-                fp:write(item(get_node_lines(db, li)))
+                fp:write(backend.item(get_node_lines(db, li)))
             end
-            fp:write(end_list())
+            fp:write(backend.end_list())
         end
     end
 
 end
 
-function projects(fp)
-    fp:write(header2("Projects"))
+function projects(fp, backend)
+    fp:write(backend.header2("Projects"))
     local node_name = "resume/projects"
     local nid = get_node_id(db, node_name)
     assert(nid > 0,
@@ -130,12 +137,12 @@ function projects(fp)
 
     for _, v in pairs(explst) do
         local title = get_node_lines(db, v)
-        fp:write(header3(title))
+        fp:write(backend.header3(title))
     end
 end
 
-function skills(fp)
-    fp:write(header2("Skills"))
+function skills(fp, backend)
+    fp:write(backend.header2("Skills"))
     local node_name = "resume/skills"
     local nid = get_node_id(db, node_name)
     assert(nid > 0,
@@ -144,16 +151,16 @@ function skills(fp)
 
     local explst = get_node_list(db, nid)
 
-    fp:write(begin_list())
+    fp:write(backend.begin_list())
     for _, v in pairs(explst) do
         local skillset = get_node_lines(db, v)
-        fp:write(item(skillset))
+        fp:write(backend.item(skillset))
     end
-    fp:write(end_list())
+    fp:write(backend.end_list())
 end
 
-function education(fp)
-    fp:write(header2("Education"))
+function education(fp, backend)
+    fp:write(backend.header2("Education"))
 
     local node_name = "resume/education"
     local nid = get_node_id(db, node_name)
@@ -163,24 +170,29 @@ function education(fp)
 
     local explst = get_node_list(db, nid)
 
-    fp:write(begin_list())
+    fp:write(backend.begin_list())
     for _, v in pairs(explst) do
         local skillset = get_node_lines(db, v)
-        fp:write(item(skillset))
+        fp:write(backend.item(skillset))
     end
-    fp:write(end_list())
+    fp:write(backend.end_list())
 end
 
-function generate(fp)
-    fp:write(header1("Resume"))
-    experience(md)
-    projects(md)
-    skills(md)
-    education(md)
+function generate(fp, backend)
+    fp:write(backend.prelude())
+    fp:write(backend.header1("Paul Batchelor"))
+    experience(fp, backend)
+    projects(fp, backend)
+    skills(fp, backend)
+    education(fp, backend)
+    fp:write(backend.epilogue())
 end
 
 md = io.open("out.md", "w")
-
-generate(md)
-
+generate(md, markdown_backend)
 md:close()
+
+tex = io.open("out.tex", "w")
+generate(tex, tex_backend)
+tex:close()
+
