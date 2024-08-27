3e000de4a6f23e9383245879b52303b7e0c296bb
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-08-27 14:53:43 -0400

    added editorial solution

--- leetcode/2462/total_cost_k_workers.py
+++ leetcode/2462/total_cost_k_workers.py
@@ -4,6 +4,7 @@
 # problem, so I get the feeling that I'm missing something
 # in my approach because mine is essentially brute force
 
+# note: candidates/k have been swapped
 def brute_force(costs, candidates, k):
     k = min(k, len(costs))
     sum = 0
@@ -44,7 +45,56 @@ def brute_force(costs, candidates, k):
 
     return sum
 
+
+# 2 priority solution, adapted from the editorial
+
+import heapq
+
+def pq2(costs, k, candidates):
+    m = candidates
+    head_workers = []
+    tail_workers = []
+
+    next_head = m
+    next_tail = len(costs) - m - 1
+    sum = 0
+    heapq.heapify(head_workers)
+    heapq.heapify(tail_workers)
+
+    for i in range(0, m):
+        heapq.heappush(head_workers, costs[i])
+        heapq.heappush(tail_workers, costs[len(costs) - 1 - i])
+
+    for _ in range(0, k):
+        # is there a way to peek?
+        head = heapq.heappop(head_workers)
+        tail = heapq.heappop(tail_workers)
+
+        if head == tail:
+            # put tail back
+            heapq.heappush(tail_workers, tail)
+            heapq.heappush(head_workers, costs[next_head])
+            next_head += 1
+            sum += head
+        elif head < tail:
+            heapq.heappush(tail_workers, tail)
+            heapq.heappush(head_workers, costs[next_head])
+            next_head += 1
+            sum += head
+        else:
+            heapq.heappush(head_workers, head)
+            heapq.heappush(tail_workers, costs[next_tail])
+            next_tail -= 1
+            sum += tail
+
+    return sum
+
+
+
 out = brute_force([17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4)
+assert(out == 11)
+
+out = pq2([17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4)
 assert(out == 11)
 
 out = brute_force([1, 2, 4, 1], 3, 3)
