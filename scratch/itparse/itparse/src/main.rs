97d94bc68ab86e91d275bc7db8a6f6d528adf97b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-09-11 13:14:41 -0400

    itparse stuff. dump too

--- scratch/itparse/itparse/src/main.rs
+++ scratch/itparse/itparse/src/main.rs
@@ -55,64 +55,20 @@ impl ImpulseTrackerModule {
     }
 }
 
-fn main() -> io::Result<()> {
-    let mut f = File::open("../small2.it")?;
-    let mut buffer = Vec::new();
-
-    f.read_to_end(&mut buffer)?;
-
-    println!("{}", buffer.len());
-
-    //dbg!(&buffer[0..4]);
-
-    let str = str::from_utf8(&buffer[0..4]).unwrap();
-    //dbg!(str);
-
-    let title = str::from_utf8(&buffer[0x4..0xf]).unwrap();
-    //dbg!(title);
-
-    // OrdNum
-    //dbg!(&buffer[0x20..0x22]);
-    let ord_num: u16 = buffer[0x20] as u16 | ((buffer[0x21] as u16) << 8);
-    //dbg!(ord_num);
-    let ins_num: u16 = buffer[0x22] as u16 | ((buffer[0x23] as u16) << 8);
-    //dbg!(ins_num);
-    let smp_num: u16 = buffer[0x24] as u16 | ((buffer[0x25] as u16) << 8);
-    //dbg!(smp_num);
-    let pat_num: u16 = buffer[0x26] as u16 | ((buffer[0x27] as u16) << 8);
-    //dbg!(pat_num);
-    let cwt_v: u16 = buffer[0x28] as u16 | ((buffer[0x29] as u16) << 8);
-    //dbg!(cwt_v);
-    //let ord_num = &buffer[0x20..0x22].from_le();
-
-    let pat_off = 0x00C0 + ord_num + ins_num * 4 + smp_num * 4;
+fn parse_pattern(
+    buffer: &[u8],
+    pat_num: u16,
+    ord_num: u16,
+    ins_num: u16,
+    smp_num: u16,
+) -> Vec<PatternCellData> {
+    let pat_off = 0x00C0 + ord_num + ins_num * 4 + smp_num * 4 + pat_num * 4;
     let pat_off = pat_off as usize;
-    //let pat_off = buffer[pat_off]
-    //    | (buffer[pat_off + 1] << 8)
-    //    | (buffer[pat_off + 2] << 16)
-    //    | (buffer[pat_off + 3] << 24);
 
-    //dbg!(&buffer[pat_off..(pat_off + 4)]);
-    let pat_off = u32::from_le_bytes([
-        buffer[pat_off],
-        buffer[pat_off + 1],
-        buffer[pat_off + 2],
-        buffer[pat_off + 3],
-    ]) as usize;
-    //dbg!(pat_off);
-    // // dbg!(&buffer[0x20..0x22]);
+    let pat_off = u32::from_le_bytes(buffer[pat_off..(pat_off + 4)].try_into().unwrap()) as usize;
 
-    //dbg!(&buffer[pat_off..(pat_off + 8)]);
     let pat_len = buffer[pat_off] as u16 | ((buffer[pat_off + 1] as u16) << 8);
-    //dbg!(pat_len);
     let pat_rows = buffer[pat_off + 2] as u16 | ((buffer[pat_off + 3] as u16) << 8);
-    //dbg!(pat_rows);
-    // dbg!(&buffer[0xC0..0xCF]);
-    // dbg!(&buffer[0xD0..0xDF]);
-    //
-
-    // read pattern 1 data
-
     let mut i = 0;
     let pat_start = pat_off + 8;
     let mut prev_mask_variable = 0;
@@ -231,9 +187,47 @@ fn main() -> io::Result<()> {
         });
     }
 
-    dbg!(pat);
-    dbg!(max_chan);
-    dbg!(ncells);
+    pat
+}
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("../small2.it")?;
+    let mut buffer = Vec::new();
+
+    f.read_to_end(&mut buffer)?;
+
+    println!("{}", buffer.len());
+
+    //dbg!(&buffer[0..4]);
+
+    let str = str::from_utf8(&buffer[0..4]).unwrap();
+    //dbg!(str);
+
+    let title = str::from_utf8(&buffer[0x4..0xf]).unwrap();
+    //dbg!(title);
+
+    // OrdNum
+    //dbg!(&buffer[0x20..0x22]);
+    let ord_num: u16 = buffer[0x20] as u16 | ((buffer[0x21] as u16) << 8);
+    //dbg!(ord_num);
+    let ins_num: u16 = buffer[0x22] as u16 | ((buffer[0x23] as u16) << 8);
+    //dbg!(ins_num);
+    let smp_num: u16 = buffer[0x24] as u16 | ((buffer[0x25] as u16) << 8);
+    //dbg!(smp_num);
+    let pat_num: u16 = buffer[0x26] as u16 | ((buffer[0x27] as u16) << 8);
+    //dbg!(pat_num);
+    let cwt_v: u16 = buffer[0x28] as u16 | ((buffer[0x29] as u16) << 8);
+    //dbg!(cwt_v);
+    //let ord_num = &buffer[0x20..0x22].from_le();
+
+    let mut patterns = vec![];
+    for i in 0..pat_num {
+        let pat = parse_pattern(&buffer, i, ord_num, ins_num, smp_num);
+        patterns.push(pat);
+    }
+
+    dbg!(patterns.len());
+
     Ok(())
 }
 
