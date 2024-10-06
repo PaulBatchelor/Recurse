50e4c53e524e970a7b0283fb135061b37512cf5a
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-10-06 15:25:58 -0400

    LC 236

---
+++ leetcode/236/LCA.py
@@ -0,0 +1,96 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+    def dfs(self, node, target):
+        if node is None:
+            return False
+        if node.val == target.val:
+            return True
+        return self.dfs(node.left, target) or self.dfs(node.right, target)
+    
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        
+        print(root.val)
+        self.stack.append(root)
+        if root.val in (p.val, q.val):
+            other = p
+            print(f"found {root.val}")
+            if root.val == p.val:
+                other = q
+            stack = self.stack
+            while len(stack) > 0:
+                n = stack.pop()
+                if self.dfs(n, other):
+                    return n
+        #self.stack.append(root)
+        return self.lowestCommonAncestor(root.left, p, q) or self.lowestCommonAncestor(root.right, p, q)
+    def lowestCommonAncestor3(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+
+        if root.val in (p.val, q.val):
+            return root
+        # print(root.val, p.val, q.val)
+
+        if root.left and root.left.val in (p.val, q.val):
+            # LCA is either root or left
+            selected = p
+            if root.left.val == selected.val:
+                selected = q
+            # print(f"found: {selected}")
+            # setting p == q is a bit of a hack that allows for a DFS single-value search
+            a = self.lowestCommonAncestor(root.left, selected, selected)
+            if a is None:
+                return root
+            return root.left
+        
+        # do similar check for righthand side
+        if root.right and root.right.val in (p.val, q.val):
+            # LCA is either root or left
+            selected = p
+            if root.right.val == selected.val:
+                selected = q
+            # setting p == q is a bit of a hack that allows for a DFS single-value search
+            a = self.lowestCommonAncestor(root.right, selected, selected)
+            if a is None:
+                return root
+            return root.right
+        
+        # neither p or q found, traverse left/right
+        return self.lowestCommonAncestor(root.left, p, q) or self.lowestCommonAncestor(root.right, p, q)
+            
+    def lowestCommonAncestor2(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # check and see if children are p and q
+        left = root.left
+        right = root.right
+
+        if left and (left.val == q or left.val == p):
+            searchfor = q                
+            if left.val == q:
+                searchfor = p
+            if self.dfs(left, searchfor):
+                return left.val
+            else:
+                return root.val
+        
+        if right:
+            if right.val == p:
+                pass
+            elif right.val == q:
+                pass
+
+        # find the corresponding node using DFS
+        # if the other node is in the opposite child,
+        # then the LCA is the current parent, otherwise
+        # it is the corresponding child node
+        
+        return None
