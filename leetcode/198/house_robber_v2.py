1bb27392464e3b79b68d2e22309bf575a1b415c0
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-11-12 10:14:03 -0500

    grind75

---
+++ leetcode/198/house_robber_v2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob_r(self, nums: List[int], pos: int)->int:
+        if pos >= len(nums):
+            return 0
+        if self.cache[pos] >= 0:
+            return self.cache[pos]
+        v1 = nums[pos] + self.rob_r(nums, pos + 2)
+        v2 = 0
+        if (pos + 1) < len(nums):
+            v2 = nums[pos + 1] + self.rob_r(nums, pos + 3)
+        self.cache[pos] = max(v1, v2)
+        return self.cache[pos]
+    def rob(self, nums: List[int]) -> int:
+        self.cache = [-1]*len(nums)
+        return self.rob_r(nums, 0)
