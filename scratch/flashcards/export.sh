76c8c294ad85de06c803eea361c8f9ba4c5c4872
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-11-21 14:14:55 -0500

    flashcards: export script writes to file instead of stdout

--- scratch/flashcards/export.sh
+++ scratch/flashcards/export.sh
@@ -1,6 +1,9 @@
-sqlite3 ../../a.db <<EOM
+OUTFILE="cards.tsv"
+sqlite3 ../../a.db > $OUTFILE <<EOM
 .mode tabs
 SELECT name, level, num_correct from flashcard_metadata
 ORDER BY name ASC;
 ;
 EOM
+
+echo "Exported data to $OUTFILE"
