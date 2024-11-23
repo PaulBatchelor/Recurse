c19d910cc61b3430d58da38d7cbf513a9dc71179
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-11-23 18:02:08 -0500

    LC20

--- leetcode/20/valid_parens.py
+++ leetcode/20/valid_parens.py
@@ -1,3 +1,40 @@
+# 2024-11-23: This one I almost got right on the first go
+# except for the edge case where there'd be too many opens
+# I missed this the last time I attempted this, but didn't
+# catch it since I didn't submit it
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # length of string must be multiple of 2 to be valid
+        if len(s) % 2 == 1:
+            return False
+
+        stk = []
+
+        # linear sweep through string
+        for p in s:
+            if p in "([{":
+                stk.append(p)
+                continue
+            # attmpt to see if closing paren matches
+            if len(stk) == 0:
+                return False
+
+            open_paren = stk.pop()
+
+            if open_paren == "(" and p == ")":
+                continue
+            elif open_paren == "{" and p == "}":
+                continue
+            elif open_paren == "[" and p == "]":
+                continue
+            else:
+                return False
+
+        # non-zero items on the stack implies it is unbalanced
+        return len(stk) == 0
+
+# old non-LC version
+
 def valid_parens(s):
     stack = []
     lparens = {'{', '[', '('}
