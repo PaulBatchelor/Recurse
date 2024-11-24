34cb8e15a6de5dfb47d0a1ca58dca25c5aa3f150
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-11-24 10:19:13 -0500

    LC 125

--- leetcode/125/valid_palindrome.py
+++ leetcode/125/valid_palindrome.py
@@ -1,3 +1,43 @@
+# 2024-11-24: two-pointer solution, with compare with
+# filtered reverse string approach
+class Solution:
+    def isPalindrome_reversed(self, s: str) -> bool:
+        filtered_chars = filter(lambda ch: ch.isalnum(), s)
+        lowercase_filtered_chars = map(lambda ch: ch.lower(), filtered_chars)
+        filtered_chars_list = list(lowercase_filtered_chars)
+        reversed_chars_list = filtered_chars_list[::-1]
+
+        return filtered_chars_list == reversed_chars_list
+    # my version, conventional two-pointer wisdom
+    def isPalindrome(self, s: str) -> bool:
+        # convert to lowercase for case insensitivity
+        s = s.lower()
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            # check if left/right characters are non-alphanumeric, and move
+            # the pointers
+
+            if not (s[left].isalpha() or s[left].isnumeric()):
+                left += 1
+                continue
+            if not (s[right].isalpha() or s[right].isnumeric()):
+                right -= 1
+                continue
+
+            # at this point, ends are alphanumeric characters, check and see if
+            # they match. If they don't, it is not a palindrome
+
+            if s[left] != s[right]:
+                return False
+
+            # pairs match, move both pointers
+            left += 1
+            right -= 1
+        return True
+
 # Valid palindrome: the intuitive way to do this is to
 # sweep through the string and check the beginnings/endings
 # to ensure they match. Python has built-in ways to remove
