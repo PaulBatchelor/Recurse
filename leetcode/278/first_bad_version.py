f93cf5b2a5a7ef14f4412e86039362dc807e7645
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-11-25 13:06:44 -0500

    LC 278

--- leetcode/278/first_bad_version.py
+++ leetcode/278/first_bad_version.py
@@ -1,3 +1,21 @@
+# 2024-11-25 My revisited version. Turns out I did this
+# already oops
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
 # 10/11 My first solution was a little more verbose
 # than this, with unnecessary checks. I fed it through
 # claude, and it gave me the more optimal approach here:
