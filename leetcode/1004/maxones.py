c90d71c4e8e0ffc84e66458a4176abc6d66e827c
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-12-06 08:48:53 -0500

    LC1004

--- leetcode/1004/maxones.py
+++ leetcode/1004/maxones.py
@@ -1,47 +1,48 @@
-# This one does NOT work at all. It is completely
-# broken and incorrect.
-def attempt(nums, k):
-    L = 0
-    count = 0
-    start = 0
+# 2024-12-06 Poured a few hours into a solution using
+# a deque, then looked at the editorial
 
-    if nums[0] == 0:
-        count += 1
+from collections import deque
+class Solution:
+    # editorial
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        for right in range(len(nums)):
+            k -= 1 - nums[right]
 
-    maxlen = 0
+            if k < 0:
+                k += 1 - nums[left]
+                left += 1
 
-    for i in range(1, len(nums)):
-        if nums[i] == 0:
-            if count == k:
-                count -= 1
-                start = L + 1
-                
-                for x in range(start, len(nums)):
-                    if nums[x] == 0:
-                        L = x
-                        break;
+        return right - left + 1
 
-        maxlen = max(maxlen, i - start)
-    return maxlen
+    # what I hacked together
+    def longestOnesV1(self, nums: List[int], k: int) -> int:
+        q = deque()
+        start = 0
+        end = 0
 
+        for i in range(len(nums)):
+            if len(q) == k:
+                break
+            if nums[i] == 0:
+                q.append(i)
+            end = i + 1
 
-# currently incorrect, even though it's allegedly ported
-# from the C++ answer
-def editorial(nums, k):
-    L = 0
-    for R in range(0, len(nums)):
-        if nums[R] == 0:
-            k -= 1
-        if k < 0:
-            k += 1 - nums[L]
-            L += 1
-    return R - L
+        maxones = (end - start)
 
-nums = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1]
-k = 3
+        off = end
 
-out = attempt(nums, k)
-assert(out == 10)
+        for i in range(off, len(nums)):
+            if k == 0 and nums[i] == 0:
+                start = end = -1
+                continue
+            if nums[i] == 1:
+                if start < 0:
+                    start = i
+            if nums[i] == 0:
+                start = q.popleft() + 1
+                q.append(i)
+            end = i + 1
+            maxones = max((end - start), maxones)
 
-# out = editorial(nums, k)
-# assert(out == 10)
+        return maxones
