cf68c4403df0605bd83ca400759d0a2ca8c45d39
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-01-01 15:28:37 -0500

    LC 1143

--- leetcode/1143/longest_common_subseq.py
+++ leetcode/1143/longest_common_subseq.py
@@ -1,3 +1,43 @@
+# 2025-01-01: Following dynamic programming explore card
+
+class Solution:
+    # bottoms up
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        t1len, t2len = len(text1), len(text2)
+
+        dp = [[0] * (t2len + 1) for _ in range(t1len + 1)]
+
+        for i in reversed(range(t1len)):
+            for j in reversed(range(t2len)):
+                if text1[i] == text2[j]:
+                    dp[i][j] = max(1 + dp[i + 1][j + 1], dp[i][j + 1], dp[i + 1][j + 1])
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return dp[0][0]
+
+    def longestCommonSubsequenceV1(self, text1: str, text2: str) -> int:
+        t1len, t2len = len(text1), len(text2)
+        @cache
+        def dp(i, j):
+            if i == t1len:
+                return 0
+            if j == t2len:
+                return 0
+
+            if text1[i] == text2[j]:
+                match_one = 1 + dp(i + 1, j + 1)
+                match_two = dp(i, j + 1)
+                match_three = dp(i + 1, j)
+                return max(match_one, match_two, match_three)
+
+            x = dp(i, j + 1)
+            y = dp(i + 1, j)
+            return max(x, y)
+
+        return dp(0, 0)
+
+
 # 2024-12-27 So close to getting it right
 
 class Solution:
